a79652cd10b29bddcff7395dd1adf0525d74f46b
José Claudio Alves <you@example.comCLEAR>
2018-08-14 18:40:06 -0300

    criado HTMLs

---
+++ pratica/HTML_CSS_Bootstrap/modules/bootstrap/dist/js/script.js
@@ -0,0 +1,59 @@
+
+
+// Scroll //
+
+(function(){
+$('#menu a').click(function(e) {
+
+  var id= $(this).attr('href'),
+  targetOffset = $(id).offset().top, 
+  menuHeight = $('#main-menu').innerHeight();
+  $('html, body').animate({
+    scrollTop: targetOffset -menuHeight 
+  },1000);
+});
+}());
+
+// Animação //
+
+debounce = function(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+
+(function(){
+  var $target = $('.anime'),
+      animationClass = 'anime-start',
+      offset = $(window).height() * 3/4;
+
+  function animeScroll() {
+    var documentTop = $(document).scrollTop();
+
+    $target.each(function(){
+      var itemTop = $(this).offset().top;
+      if (documentTop > itemTop - offset) {
+        $(this).addClass(animationClass);
+      } else {
+        $(this).removeClass(animationClass);
+      }
+    });
+  }
+
+  animeScroll();
+
+  $(document).scroll(debounce(function(){
+    animeScroll();
+    console.log('teste');
+  }, 200));
+})();
